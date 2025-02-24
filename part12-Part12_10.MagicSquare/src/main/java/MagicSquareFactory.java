545bd0b33cf3661f9e4cb4d8232e4cdf3eee26c8
Jael Bohorquez <Jaelbkz@icloud.com>
2025-02-24 05:03:36 -0600

    modify:  part12-Part12_10.MagicSquare

--- part12-Part12_10.MagicSquare/src/main/java/MagicSquareFactory.java
+++ part12-Part12_10.MagicSquare/src/main/java/MagicSquareFactory.java
@@ -2,11 +2,35 @@
 public class MagicSquareFactory {
 
     public MagicSquare createMagicSquare(int size) {
-
         MagicSquare square = new MagicSquare(size);
-
-        // implement the creation of a magic square with the Siamese method algorithm here
+        int freePositions = size * size;
+        int number = 1;
+        int posX = 0;
+        int posY = size / 2;
+        
+        while(freePositions-- > 0){
+            if(!isValidPosition(posX, posY, size)){
+                posX = size - 2;
+                posY --;
+            }
+            
+            if(isFreePosition(posY, posX, square)){
+                square.placeValue(posY, posX, number++);
+                posX += size - 1;
+                posY ++;
+            }
+        }
         return square;
     }
+    
+    private boolean isValidPosition(int posX, int posY, int size){
+        return posX < size && posY < size;
+    }
+    
+    private boolean isFreePosition(int posX, int posY, MagicSquare ms){
+        return ms.readValue(posY, posX) != - 1;
+    }
+    
+    
 
 }
